8fbd5372934de0b366f98c70b9ae1bd4ff5229f0
BennyZheng203 <157761504+BennyZheng203@users.noreply.github.com>
2024-11-13 14:09:14 +0000

    added unique_dir

--- Pipeline/atlas_query.py
+++ Pipeline/atlas_query.py
@@ -43,11 +43,11 @@ class Atclean_Query():
                 self.run_script_with_args(download_script, *download_args)
 
                 # Run `clean.py` with its specific flags (modify as needed)
-                clean_args = [name, '-x', '-p', '-o', '-g']
-                self.run_script_with_args(clean_script, *clean_args)
+                #clean_args = [name, '-x', '-p', '-o', '-g']
+                #self.run_script_with_args(clean_script, *clean_args)
 
 if __name__ == "__main__":
-    repo_path = r'/users/jhzhe/Cloned_Repos/ATCleanRepo'
+    repo_path = r'/users/jhzhe/Cloned_Repos/ATCleanRepoTest'
     ned_path = r'/users/jhzhe/Cloned_Repos/MPhys-Project/output_data/ned_search/'
     data_path = [os.path.join(ned_path, csv) for csv in os.listdir(ned_path) if csv.endswith(".csv")]
 
